b80e5eae44bebe55981602069313492f13c37616
Mancy <519660797@qq.com>
2017-03-09 10:03:05 +0800

    登录界面

---
+++ app/src/main/java/com/Linmingming/welcome/WelcomeActivity.java
@@ -0,0 +1,50 @@
+package com.Linmingming.welcome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+import com.Linmingming.MainActivity;
+import com.Linmingming.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startMainActivity();
+        }
+        return super.onTouchEvent(event);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+}

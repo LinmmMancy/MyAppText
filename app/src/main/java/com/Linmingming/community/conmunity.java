c7e8c3434c883ec2f5cca67fd4e9e8a137038ea6
Mancy <519660797@qq.com>
2017-03-09 09:53:08 +0800

    框架搭建

---
+++ app/src/main/java/com/Linmingming/community/conmunity.java
@@ -0,0 +1,23 @@
+package com.Linmingming.community;
+
+import android.view.View;
+
+import com.Linmingming.base.BaseFragment;
+
+/**
+ * Created by Mancy on 2017/3/9.
+ */
+
+public class conmunity extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+    }
+}
---
+++ app/src/main/java/com/Linmingming/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.Linmingming.home;
+
+import android.view.View;
+
+import com.Linmingming.base.BaseFragment;
+
+/**
+ * Created by Mancy on 2017/3/9.
+ */
+
+public class HomeFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+
+    }
+}
